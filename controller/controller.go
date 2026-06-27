9f2e08d51dc7bb3635f0fa4568b887def8c0b472
Karel Van Hecke <25814162+karelvanhecke@users.noreply.github.com>
2026-06-27 15:42:33 +0200

    add GetCapacity call

--- controller/controller.go
+++ controller/controller.go
@@ -242,6 +242,29 @@ func (cs *ControllerServer) ValidateVolumeCapabilities(ctx context.Context, req 
 	return resp, nil
 }
 
+func (cs *ControllerServer) GetCapacity(ctx context.Context, req *csi.GetCapacityRequest) (*csi.GetCapacityResponse, error) {
+	params := req.GetParameters()
+
+	pool, err := cs.lookupStoragePool(params)
+	if err != nil {
+		return nil, err
+	}
+
+	c, err := cs.connectedClient()
+	if err != nil {
+		return nil, err
+	}
+
+	_, _, _, a, err := c.StoragePoolGetInfo(pool)
+	if err != nil {
+		return nil, grpcerr.Internal(err)
+	}
+
+	return &csi.GetCapacityResponse{
+		AvailableCapacity: int64(a),
+	}, nil
+}
+
 // Ensure the Libvirt client is connected.
 // Returns grpc error code unavailable on connection issues.
 func (cs *ControllerServer) connectedClient() (*libvirt.Libvirt, error) {
