1a0a041bf0a4c621956874a3d836890bb0c5ece5
Karel Van Hecke <25814162+karelvanhecke@users.noreply.github.com>
2026-06-27 16:25:44 +0200

    add ControllerExpandVolume

--- controller/controller.go
+++ controller/controller.go
@@ -265,6 +265,86 @@ func (cs *ControllerServer) GetCapacity(ctx context.Context, req *csi.GetCapacit
 	}, nil
 }
 
+func (cs *ControllerServer) ControllerExpandVolume(ctx context.Context, req *csi.ControllerExpandVolumeRequest) (*csi.ControllerExpandVolumeResponse, error) {
+	c, err := cs.connectedClient()
+	if err != nil {
+		return nil, err
+	}
+
+	vol, err := c.StorageVolLookupByKey(req.GetVolumeId())
+	if err != nil {
+		if isVolNotFoundError(err) {
+			return nil, grpcerr.NotFound(err)
+		}
+		return nil, grpcerr.Internal(err)
+	}
+
+	requestedBytes := req.CapacityRange.RequiredBytes
+	_, volCapacity, _, err := c.StorageVolGetInfo(vol)
+	if err != nil {
+		return nil, grpcerr.Internal(err)
+	}
+
+	if c := int64(volCapacity); c > requestedBytes {
+		return &csi.ControllerExpandVolumeResponse{CapacityBytes: c, NodeExpansionRequired: false}, nil
+	}
+
+	pool, err := c.StoragePoolLookupByVolume(vol)
+	if err != nil {
+		if isPoolNotFoundError(err) {
+			return nil, grpcerr.InvalidArgument(err)
+		}
+
+		return nil, grpcerr.Internal(err)
+	}
+	_, _, _, poolCapacity, err := c.StoragePoolGetInfo(pool)
+	if err != nil {
+		return nil, grpcerr.Internal(err)
+	}
+
+	if requestedBytes > int64(poolCapacity) {
+		return nil, grpcerr.InvalidArgument(errors.New("request exceeds available storage pool capacity"))
+	}
+
+	volXML, err := c.StorageVolGetXMLDesc(vol, 0)
+	if err != nil {
+		return nil, grpcerr.Internal(err)
+	}
+
+	volSpec := &libvirtxml.StorageVolume{}
+	if err := volSpec.Unmarshal(volXML); err != nil {
+		return nil, grpcerr.AlreadyExists(err)
+	}
+
+	dom, ok, err := cs.domainUsingVolume(volSpec)
+	if err != nil {
+		return nil, err
+	}
+
+	if !ok {
+		if err := c.StorageVolResize(vol, uint64(requestedBytes), 0); err != nil {
+			return nil, grpcerr.Internal(err)
+		}
+	} else {
+		if err := c.DomainBlockResize(dom, volSpec.Target.Path, uint64(requestedBytes), libvirt.DomainBlockResizeBytes); err != nil {
+			return nil, grpcerr.Internal(err)
+		}
+	}
+
+	resp := &csi.ControllerExpandVolumeResponse{
+		CapacityBytes: requestedBytes,
+	}
+
+	if cap := req.VolumeCapability; cap != nil {
+		if _, ok := cap.AccessType.(*csi.VolumeCapability_Block); ok {
+			resp.NodeExpansionRequired = false
+		}
+		resp.NodeExpansionRequired = true
+	}
+
+	return resp, nil
+}
+
 // Ensure the Libvirt client is connected.
 // Returns grpc error code unavailable on connection issues.
 func (cs *ControllerServer) connectedClient() (*libvirt.Libvirt, error) {
@@ -382,8 +462,7 @@ func (cs *ControllerServer) lookupStoragePool(params map[string]string) (libvirt
 	if err == nil {
 		return pool, nil
 	}
-	e, ok := err.(libvirt.Error)
-	if ok && e.Code == uint32(libvirt.ErrNoStoragePool) {
+	if isPoolNotFoundError(err) {
 		return libvirt.StoragePool{}, grpcerr.InvalidArgument(err)
 	}
 	return libvirt.StoragePool{}, grpcerr.Internal(err)
@@ -519,28 +598,46 @@ func (cs *ControllerServer) attachToDomain(vol *libvirtxml.StorageVolume, dom li
 		return grpcerr.Internal(err)
 	}
 
-	doms, _, err := c.ConnectListAllDomains(1, libvirt.ConnectListDomainsActive)
+	dom, ok, err = cs.domainUsingVolume(vol)
+	if err != nil {
+		return err
+	}
+
+	if !ok {
+		return grpcerr.Internal(e)
+	}
+
+	id, err := uuid.ParseBytes(dom.UUID[:])
 	if err != nil {
 		return grpcerr.Internal(err)
+	}
+
+	return status.Error(codes.FailedPrecondition, id.String())
+}
+
+func (cs *ControllerServer) domainUsingVolume(vol *libvirtxml.StorageVolume) (dom libvirt.Domain, ok bool, err error) {
+	c, err := cs.connectedClient()
+	if err != nil {
+		return libvirt.Domain{}, false, err
+	}
+
+	doms, _, err := c.ConnectListAllDomains(1, libvirt.ConnectListDomainsActive)
+	if err != nil {
+		return libvirt.Domain{}, false, grpcerr.Internal(err)
 	}
 
 	for _, dom := range doms {
 		_, ok, _, err := cs.isAttachedToDomain(vol, dom)
 		if err != nil {
-			return grpcerr.Internal(err)
+			return libvirt.Domain{}, false, grpcerr.Internal(err)
 		}
 
 		if ok {
-			id, err := uuid.ParseBytes(dom.UUID[:])
-			if err != nil {
-				return grpcerr.Internal(err)
-			}
-
-			return status.Error(codes.FailedPrecondition, id.String())
+			return dom, true, nil
 		}
 	}
 
-	return grpcerr.Internal(e)
+	return libvirt.Domain{}, false, nil
 }
 
 func domainDisk(vol *libvirtxml.StorageVolume, wwn string, dev string) (string, error) {
@@ -602,6 +699,14 @@ func verifyCapacity(cap int64, capRange *csi.CapacityRange) error {
 func isVolNotFoundError(err error) bool {
 	e, ok := err.(libvirt.Error)
 	if !ok || e.Code != uint32(libvirt.ErrNoStorageVol) {
+		return false
+	}
+	return true
+}
+
+func isPoolNotFoundError(err error) bool {
+	e, ok := err.(libvirt.Error)
+	if !ok || e.Code != uint32(libvirt.ErrNoStoragePool) {
 		return false
 	}
 	return true
